9d0156ca32ab61bbc3ec2d39783c5ee06fdd1a08
christopher coyne <christcoyne@gmail.com>
2022-07-17 22:02:33 -0400

    onclick added for butotn

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
 import { StyledButton } from "./Button.style";
+import React from "react";
 interface Props {
   input: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: React.FC<Props> = ({ input }) => {
-  return <StyledButton>{input}</StyledButton>;
+const Button: React.FC<Props> = ({ input, onClick }: Props) => {
+  return <StyledButton onClick={onClick}>{input}</StyledButton>;
 };
 
 export default Button;

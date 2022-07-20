323dc71ecfc4a0d350077b84740b0c0a336bd4fb
christopher coyne <christcoyne@gmail.com>
2022-07-19 22:18:36 -0400

    add recipe modal basic inputs added

--- src/components/AddRecipe/AddRecipe.style.tsx
+++ src/components/AddRecipe/AddRecipe.style.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled("div")`
-  text-align: left;
-`;
+export const StyledForm = styled("form")``;
+
+export const StyledTextArea = styled("textarea")``;
--- src/components/AddRecipe/AddRecipe.tsx
+++ src/components/AddRecipe/AddRecipe.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { StyledForm, StyledTextArea } from "./AddRecipe.style";
+import Button from "../Button/Button";
+import { Recipe } from "../../types";
+import types from "../../FilterOptionTypes";
 
 const AddRecipe = () => {
-  return <div>AddRecipe</div>;
+  /* refs for text input */
+  const ingredients = useRef<HTMLTextAreaElement>(null);
+  const userName = useRef<HTMLInputElement>(null);
+  const instructions = useRef<HTMLTextAreaElement>(null);
+  const dishName = useRef<HTMLInputElement>(null);
+
+  const [difficulty, setDifficulty] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [type, setType] = useState("");
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+
+    // if all fields are filled out, submit the
+    if (
+      userName.current &&
+      instructions.current &&
+      ingredients.current &&
+      dishName.current
+    ) {
+      console.log(userName.current.value);
+      /*
+      const newRecipe = {
+        name: dishName.current.value,
+        ingredients: ingredients.current.value,
+        instructions: instructions,
+        difficulty: difficulty || null;
+        cuisine:
+        type:
+        user: userName.current.value
+        date: 'sample date',
+      }
+      userName.current.value = "";
+      */
+    }
+
+    /*
+    const newRecipe :Recipe = {
+      name: 
+    }
+    */
+    // close modal and reset recipes
+  };
+  return (
+    <StyledForm>
+      <h1>Submit a New Recipe</h1>
+      <label>Cuisine:</label>
+      {types.cuisine.map((cuisine: string) => (
+        <Button
+          input={cuisine}
+          onClick={() => console.log("hello")}
+          type={"small"}
+        />
+      ))}
+      <label>Difficulty:</label>
+      <label>Food Type:</label>
+      <label>Dish Name:</label>
+      <input type="text" ref={dishName}></input>
+      <label>Ingredients:</label>
+      <StyledTextArea ref={ingredients} />
+      <label>Instructions:</label>
+      <StyledTextArea ref={instructions} />
+      <label>Username:</label>
+      <input type="text" ref={userName}></input>
+      <Button input="submit" onClick={(e) => handleSubmit(e)} type="regular" />
+      <Button input="cancel" onClick={(e) => handleSubmit(e)} type="cancel" />
+    </StyledForm>
+  );
 };
 
 export default AddRecipe;

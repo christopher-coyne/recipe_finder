a4e0dd46893fbcc5fb29bf997650e14da2fdbb7a
christopher coyne <christcoyne@gmail.com>
2022-07-20 21:25:08 -0400

    add recipe adds new recipe to state

--- src/components/AddRecipe/AddRecipe.tsx
+++ src/components/AddRecipe/AddRecipe.tsx
@@ -1,10 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useContext } from "react";
 import { StyledForm, StyledTextArea } from "./AddRecipe.style";
 import Button from "../Button/Button";
 import { Recipe } from "../../types";
 import types from "../../FilterOptionTypes";
 
+import {
+  selectedRecipeContext,
+  setSelectedRecipeContext,
+} from "../../contexts/ModalContext";
+import { setModalContext } from "../../contexts/ModalContext";
+import { setRecipeContext } from "../../contexts/RecipesContext";
+
 const AddRecipe = () => {
+  const setModal = useContext(setModalContext);
+  const setRecipes = useContext(setRecipeContext);
   /* refs for text input */
   const ingredients = useRef<HTMLTextAreaElement>(null);
   const userName = useRef<HTMLInputElement>(null);
@@ -15,42 +24,55 @@ const AddRecipe = () => {
   const [cuisine, setCuisine] = useState("");
   const [type, setType] = useState("");
 
+  const [error, setError] = useState(false);
+
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
 
+    if (
+      !userName.current ||
+      !instructions.current ||
+      !ingredients.current ||
+      !dishName.current
+    ) {
+      return;
+    }
+
     // if all fields are filled out, submit the
     if (
-      userName.current &&
-      instructions.current &&
-      ingredients.current &&
-      dishName.current
+      userName.current.value &&
+      instructions.current.value &&
+      ingredients.current.value &&
+      dishName.current.value
     ) {
       console.log(userName.current.value);
-      /*
       const newRecipe = {
         name: dishName.current.value,
         ingredients: ingredients.current.value,
-        instructions: instructions,
-        difficulty: difficulty || null;
-        cuisine:
-        type:
-        user: userName.current.value
-        date: 'sample date',
-      }
+        instructions: instructions.current.value,
+        difficulty: "beginner",
+        cuisine: "american",
+        type: "breakfast",
+        user: userName.current.value,
+        date: "sample date",
+      };
       userName.current.value = "";
-      */
+      setRecipes((prev: Recipe[]) => {
+        console.log("prev : ", prev);
+        console.log("to add new recipe : ", newRecipe);
+        return [...prev, newRecipe];
+      });
+      setModal("");
+    } else {
+      setError(true);
     }
 
-    /*
-    const newRecipe :Recipe = {
-      name: 
-    }
-    */
     // close modal and reset recipes
   };
   return (
     <StyledForm>
       <h1>Submit a New Recipe</h1>
+      {error && <h2>'Please fill out all fields before submitting'</h2>}
       <label>Cuisine:</label>
       {types.cuisine.map((cuisine: string) => (
         <Button

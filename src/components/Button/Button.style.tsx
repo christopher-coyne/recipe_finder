7ffcabe6e0d08f015306949fbfae2c8608d70930
christopher coyne <christcoyne@gmail.com>
2022-07-24 13:34:06 -0400

    button styles updated, cases added

--- src/components/Button/Button.style.tsx
+++ src/components/Button/Button.style.tsx
@@ -1,7 +1,39 @@
+import { NONAME } from "dns";
 import styled from "styled-components";
 
-export const StyledButton = styled("button")<{ foodType: string }>`
-  background-color: ${(props) => props.theme.primary};
-  padding: 0.5rem 2rem;
-  border: ${(props) => props.foodType === "clicked" && "5px solid red"};
+export const StyledButton = styled("button")<{ buttonType: string }>`
+  cursor: pointer;
+  background-color: ${(props) => {
+    if (props.buttonType === "clicked") return `${props.theme.colors.primary}`;
+    if (props.buttonType === "regular") return `${props.theme.colors.primary}`;
+    if (props.buttonType === "small") return `white`;
+  }};
+  padding: ${(props) => {
+    if (props.buttonType === "regular") return ".75rem 2rem";
+    if (props.buttonType === "small") return ".3rem 1rem";
+  }};
+  border: ${(props) => {
+    if (props.buttonType === "clicked") return "5px solid red";
+    if (props.buttonType === "small")
+      return `2px solid ${props.theme.colors.secondary}`;
+    return "none";
+  }};
+  border-radius: 0.6rem;
+  color: ${(props) => {
+    if (props.buttonType === "regular") return "white";
+    if (props.buttonType === "small") return props.theme.colors.secondary;
+  }};
+  font-size: ${(props) => {
+    if (props.buttonType === "small" || props.buttonType === "regular")
+      return props.theme.fontSizes.medium;
+  }};
+  font-weight: ${(props) => {
+    if (props.buttonType === "small") return props.theme.fontSizes.medium;
+  }};
+`;
+
+export const CloseButton = styled("div")`
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  display: inline;
+  margin-right: 0.2rem;
 `;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,22 @@
-import { StyledButton } from "./Button.style";
+import { StyledButton, CloseButton } from "./Button.style";
 import React from "react";
 interface Props {
   input: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-  type: "small" | "regular" | "cancel" | "clicked";
+  type:
+    | "small"
+    | "regular"
+    | "cancel"
+    | "clicked"
+    | "option"
+    | "unstyled"
+    | "unstyledClicked";
 }
 
 const Button = ({ input, onClick, type }: Props) => {
   return (
-    <StyledButton onClick={onClick} foodType={type}>
+    <StyledButton onClick={onClick} buttonType={type}>
+      {type === "small" && <CloseButton>X</CloseButton>}
       {input}
     </StyledButton>
   );

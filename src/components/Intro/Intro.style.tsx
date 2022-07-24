0506a66d06ad73a5fbb22e1a152ef3573a7d87c0
christopher coyne <christcoyne@gmail.com>
2022-07-24 01:59:51 -0400

    intro styles updated

--- src/components/Intro/Intro.style.tsx
+++ src/components/Intro/Intro.style.tsx
@@ -4,4 +4,15 @@ import { colors } from "../../colors";
 export const IntroWrapper = styled("article")`
   border: 1px solid red;
   margin: auto;
+  margin-top: 4rem;
+  text-align: left;
+
+  & h1 {
+    font-size: ${(props) => props.theme.fontSizes.largest};
+    font-weight: ${(props) => props.theme.fontWeights.regular};
+  }
+
+  & p {
+    font-size: ${(props) => props.theme.fontSizes.large};
+  }
 `;
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,7 +5,10 @@ const Intro = () => {
   return (
     <IntroWrapper>
       <h1>Recipe Finder</h1>
-      <p>Amazing recipes all in one place</p>
+      <p>
+        All of your favorite recipes in one place! Add new recipes or check out
+        some posted by other users.
+      </p>
     </IntroWrapper>
   );
 };

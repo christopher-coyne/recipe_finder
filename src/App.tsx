34919a503dda8b0551e2d776104415f06be37a31
christopher coyne <christcoyne@gmail.com>
2022-09-14 20:59:56 -0400

    put more info into themes

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,19 @@ const theme = {
   colors: {
     primary: "#613DC1",
     secondary: "#858AE3",
+    text: "#424242",
+    cancel: "#CC5A71",
+    error: "#FA718D",
   },
   fontSizes: {
-    large: "2.2rem",
-    medium: "1.25rem",
+    largest: "2.2rem",
+    large: "1.4rem",
+    medium: "1rem",
   },
   fontWeights: {
     regular: 400,
+    semiBold: 500,
+    bold: 700,
   },
 };
 

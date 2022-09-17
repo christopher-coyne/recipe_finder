5909b2c0db05fc363f1c2294a627ac1cce8d8b70
christopher coyne <christcoyne@gmail.com>
2022-09-16 23:52:18 -0400

    update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
-import Home from "./screens/Home";
-import RecipesContext from "./contexts/RecipesContext";
-import FiltersContext from "./contexts/FiltersContext";
+import { Home } from "./screens/Home";
+import RecipesContext from "contexts/RecipesContext";
+import FiltersContext from "contexts/FiltersContext";
 import { useState, useContext } from "react";
 import ReactDom from "react-dom";
-import Modal from "./Layout/Modal";
-import ModalContext from "./contexts/ModalContext";
-import FullRecipe from "./components/FullRecipe/FullRecipe";
-import AddRecipe from "./components/AddRecipe/AddRecipe";
+import Modal from "Layout/Modal";
+import ModalContext from "contexts/ModalContext";
+import { FullRecipe } from "components/FullRecipe";
+import { AddRecipe } from "components/AddRecipe";
 
 import { modalContext, setModalContext } from "./contexts/ModalContext";
 import { theme } from "./styles/theme";
